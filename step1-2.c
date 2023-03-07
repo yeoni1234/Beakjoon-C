8eec648e21c1b08797817018afb318352a547867
yeoni1234 <mc0403@naver.com>
2023-03-07 22:48:48 +0900

    first commit

---
+++ step1-2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+
+int main(void){
+
+    int A , B;
+
+    
+    scanf("%d %d", &A ,&B); //표준 입력을 받아서 변수에 저장
+    
+    printf("%d\n", A+B); //변수 내용을 출력
+
+    return 0;
+
+}
---
+++ step1-5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    int A, B;
+
+    scanf("%d %d", &A, &B);
+    printf("%g\n", (float)A/B); // float 형식 맞춰주기
+
+
+return 0;
+
+};
