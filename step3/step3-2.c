63b35dbc4a2cf896dbbac0887793499af2ccc531
yeoni1234 <mc0403@naver.com>
2023-03-20 22:13:30 +0900

    add

---
+++ step3/step3-2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+    int test;
+    scanf("%d", &test);
+
+    for(int i= 0; i<test ; i++ ){
+        int A, B;
+        scanf("%d %d", &A, &B);
+        printf("%d\n", A + B );
+        
+        
+    }
+
+
+
+    return 0;
+
+}
---
+++ step3/step3-4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main(){
+
+    int sum;
+    int type;
+    scanf("%d %d", &sum, &type);
+
+    for(int i = 0; i <= sum; i++){
+        int price;  
+        int count;
+        scanf("%d %d", &price, &count);
+
+    }
+
+
+    return 0;
+
+}
